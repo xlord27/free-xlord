0126267c55bc190a0ba7e5af67620896a61118fb
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-06 15:30:21 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         ^ THANKS FOR USING SC VVIP ^        #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By XLORD STORE                           #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# MY PATHNER                                      #"
+echo -e "================================================="
